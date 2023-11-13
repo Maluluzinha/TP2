6ed2166cfb1ddf9e48f81ec424bc80c513386e96
Maluluzinha <malulu1604@gmail.com>
2023-11-12 23:18:06 -0300

    Tirando o ipv6

--- common.c
+++ common.c
@@ -22,6 +22,40 @@ void usage(int argc, char **argv, int n) {
     }
 }
 
+void addrtostr(const struct sockaddr *addr, char *str, size_t strsize) {
+    int version;
+    char addrstr[INET6_ADDRSTRLEN + 1] = "";
+    uint16_t port;
+
+    if (addr->sa_family == AF_INET) {
+        version = 4;
+        struct sockaddr_in *addr4 = (struct sockaddr_in *)addr;
+        if (!inet_ntop(AF_INET, &(addr4->sin_addr), addrstr,
+                       INET6_ADDRSTRLEN + 1)) {
+            logexit("ntop");
+        }
+        port = ntohs(addr4->sin_port); // network to host short
+    } 
+    // else if (addr->sa_family == AF_INET6) {
+    //     version = 6;
+    //     struct sockaddr_in6 *addr6 = (struct sockaddr_in6 *)addr;
+    //     if (!inet_ntop(AF_INET6, &(addr6->sin6_addr), addrstr,
+    //                    INET6_ADDRSTRLEN + 1)) {
+    //         logexit("ntop");
+    //     }
+    //     port = ntohs(addr6->sin6_port); // network to host short
+    // } 
+    else {
+        logexit("unknown protocol family.");
+    }
+    if (str) {
+        snprintf(str, strsize, "IPv%d %s %hu", version, addrstr, port);
+    }
+}
+
+////////////////////////////////////////////
+
+//Parse do cliente:
 int addrparse(const char *addrstr, const char *portstr,
               struct sockaddr_storage *storage) {
     if (addrstr == NULL || portstr == NULL) {
@@ -43,48 +77,20 @@ int addrparse(const char *addrstr, const char *portstr,
         return 0;
     }
 
-    struct in6_addr inaddr6; // 128-bit IPv6 address
-    if (inet_pton(AF_INET6, addrstr, &inaddr6)) {
-        struct sockaddr_in6 *addr6 = (struct sockaddr_in6 *)storage;
-        addr6->sin6_family = AF_INET6;
-        addr6->sin6_port = port;
-        // addr6->sin6_addr = inaddr6
-        memcpy(&(addr6->sin6_addr), &inaddr6, sizeof(inaddr6));
-        return 0;
-    }
+    // struct in6_addr inaddr6; // 128-bit IPv6 address
+    // if (inet_pton(AF_INET6, addrstr, &inaddr6)) {
+    //     struct sockaddr_in6 *addr6 = (struct sockaddr_in6 *)storage;
+    //     addr6->sin6_family = AF_INET6;
+    //     addr6->sin6_port = port;
+    //     // addr6->sin6_addr = inaddr6
+    //     memcpy(&(addr6->sin6_addr), &inaddr6, sizeof(inaddr6));
+    //     return 0;
+    // }
 
     return -1;
 }
 
-void addrtostr(const struct sockaddr *addr, char *str, size_t strsize) {
-    int version;
-    char addrstr[INET6_ADDRSTRLEN + 1] = "";
-    uint16_t port;
-
-    if (addr->sa_family == AF_INET) {
-        version = 4;
-        struct sockaddr_in *addr4 = (struct sockaddr_in *)addr;
-        if (!inet_ntop(AF_INET, &(addr4->sin_addr), addrstr,
-                       INET6_ADDRSTRLEN + 1)) {
-            logexit("ntop");
-        }
-        port = ntohs(addr4->sin_port); // network to host short
-    } else if (addr->sa_family == AF_INET6) {
-        version = 6;
-        struct sockaddr_in6 *addr6 = (struct sockaddr_in6 *)addr;
-        if (!inet_ntop(AF_INET6, &(addr6->sin6_addr), addrstr,
-                       INET6_ADDRSTRLEN + 1)) {
-            logexit("ntop");
-        }
-        port = ntohs(addr6->sin6_port); // network to host short
-    } else {
-        logexit("unknown protocol family.");
-    }
-    if (str) {
-        snprintf(str, strsize, "IPv%d %s %hu", version, addrstr, port);
-    }
-}
-
+//Inicio do servidor:
 int server_sockaddr_init(const char *proto, const char *portstr,
                          struct sockaddr_storage *storage) {
     uint16_t port = (uint16_t)atoi(portstr); // unsigned short
@@ -100,12 +106,12 @@ int server_sockaddr_init(const char *proto, const char *portstr,
         addr4->sin_addr.s_addr = INADDR_ANY;
         addr4->sin_port = port;
         return 0;
-    } else if (0 == strcmp(proto, "v6")) {
-        struct sockaddr_in6 *addr6 = (struct sockaddr_in6 *)storage;
-        addr6->sin6_family = AF_INET6;
-        addr6->sin6_addr = in6addr_any;
-        addr6->sin6_port = port;
-        return 0;
+    // } else if (0 == strcmp(proto, "v6")) {
+    //     struct sockaddr_in6 *addr6 = (struct sockaddr_in6 *)storage;
+    //     addr6->sin6_family = AF_INET6;
+    //     addr6->sin6_addr = in6addr_any;
+    //     addr6->sin6_port = port;
+    //     return 0;
     } else {
         return -1;
     }
